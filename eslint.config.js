6ee6b1f2cf65c9ff91b1595e13293b9e18af3df1
Manuel Pages Gonzales <m.pago92@gmail.com>
2025-03-18 14:51:47 +0100

    Add prettier plugins and config - setup basic prettier configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -10,6 +10,9 @@ export default tseslint.config(
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
@@ -25,4 +28,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+);
