8053601b91837ca3c5493e6effc234a88e88cf1d
Manuel Pages Gonzales <m.pago92@gmail.com>
2025-03-19 20:50:35 +0100

    - Create fitting Results interface for fetching GIFs - Import correct result type of the useFetchGiffs hook

--- src/hooks/useFetchGifs.ts
+++ src/hooks/useFetchGifs.ts
@@ -1,21 +1,21 @@
 import { useState, useEffect } from 'react';
-import { fetchGifs } from '../services/giphyService';
-import { GIFObject } from 'giphy-api';
+import { fetchGifs, FetchGifsResults } from '../services/giphyService';
 
-interface FetchGifsHookResult {
-  data: GIFObject[] | null;
+interface UseFetchGifsResult {
+  data: FetchGifsResults | null;
   isLoading: boolean;
   error: string | null;
 }
-const useFetchGifs = (apiUrl: string, apiKey: string): FetchGifsHookResult => {
-  const [data, setData] = useState<GIFObject[] | null>(null);
+
+const useFetchGifs = (apiUrl: string, apiKey: string): UseFetchGifsResult => {
+  const [data, setData] = useState<FetchGifsResults | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-      setError(null); // Reset error before fetching
+      setError(null);
       try {
         const result = await fetchGifs(apiUrl, apiKey);
         setData(result);
--- src/services/giphyService.ts
+++ src/services/giphyService.ts
@@ -1,9 +1,23 @@
 import { GIFObject } from 'giphy-api';
 
+export interface FetchGifsResults {
+  data: GIFObject[];
+  pagination: {
+    total_count: number;
+    count: number;
+    offset: number;
+  };
+  meta: {
+    status: number;
+    msg: string;
+    response_id: string;
+  };
+}
+
 export const fetchGifs = async (
   apiUrl: string,
   apiKey: string,
-): Promise<GIFObject[]> => {
+): Promise<FetchGifsResults> => {
   const response = await fetch(`${apiUrl}?api_key=${apiKey}`);
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
